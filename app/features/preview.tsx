695a0ea8dce04af9a4022ba8cb0c32c08187601d
kayac-chang <egg734631@gmail.com>
2022-10-19 03:09:25 +0800

    feat: 🎸 add preview

---
+++ app/features/preview.tsx
@@ -0,0 +1,88 @@
+import * as PDF from "~/models/pdf";
+import { getDocument, GlobalWorkerOptions } from "pdfjs-dist";
+import type { PDFDocumentProxy, PDFPageProxy } from "pdfjs-dist";
+import { toDataURL } from "~/utils/blob";
+import { prop, range } from "ramda";
+import { wrapForSuspense } from "~/utils/react";
+import invariant from "tiny-invariant";
+import { useEffect, useRef } from "react";
+import type { RefObject } from "react";
+import { debounce } from "ts-debounce";
+
+GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.entry");
+
+function toDocument(url: string) {
+  return getDocument(url).promise;
+}
+function getAllPagesFromDocument(document: PDFDocumentProxy) {
+  return Promise.all(
+    range(1, document.numPages).map(document.getPage.bind(document))
+  );
+}
+
+const loadPages = wrapForSuspense((id: string) =>
+  PDF.getByID(id)
+    .then(prop("file"))
+    .then(toDataURL)
+    .then(toDocument)
+    .then(getAllPagesFromDocument)
+);
+
+function render(page: PDFPageProxy, ref: RefObject<HTMLDivElement>) {
+  const canvas = document.createElement("canvas");
+  const canvasContext = canvas.getContext("2d");
+
+  let isRendering = false;
+  const mount = debounce(() => {
+    if (!ref.current || isRendering) return;
+    const container = ref.current;
+
+    invariant(canvasContext);
+    const viewport = page.getViewport({
+      scale:
+        container.getBoundingClientRect().width /
+        page.getViewport({ scale: 1.0 }).width,
+    });
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+
+    isRendering = true;
+    page
+      .render({ viewport, canvasContext })
+      .promise.then(() => container.append(canvas))
+      .then(() => (isRendering = false));
+  }) as () => void;
+
+  return () => {
+    mount();
+    window.addEventListener("resize", mount);
+    return () => void window.removeEventListener("resize", mount);
+  };
+}
+
+type CanvasProps = {
+  page: PDFPageProxy;
+};
+function Canvas({ page }: CanvasProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(render(page, ref), []);
+  return <div ref={ref} />;
+}
+
+type PreviewProps = {
+  file: string;
+};
+function Preview({ file }: PreviewProps) {
+  const pages: PDFPageProxy[] = loadPages(file);
+  return (
+    <ul className="mx-auto flex max-w-screen-md flex-col gap-8">
+      {pages.map((page) => (
+        <li key={file + page.pageNumber} className="m-1 border">
+          <Canvas page={page} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Preview;
--- app/routes/documents/$id.tsx
+++ app/routes/documents/$id.tsx
@@ -1,17 +1,27 @@
 import type { LoaderArgs } from "@remix-run/server-runtime";
-import { json } from "@remix-run/server-runtime";
 import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
+import { lazy, Suspense } from "react";
+import { ClientOnly } from "remix-utils";
+const Preview = lazy(() => import("~/features/preview"));
 
 export function loader({ params }: LoaderArgs) {
   invariant(params.id);
-
-  return json({ id: params.id });
+  return params.id;
 }
 
 function Route() {
-  const { id } = useLoaderData<typeof loader>();
-  return <>{id}</>;
+  const id = useLoaderData<typeof loader>();
+
+  return (
+    <ClientOnly>
+      {() => (
+        <Suspense>
+          <Preview file={id} />
+        </Suspense>
+      )}
+    </ClientOnly>
+  );
 }
 
 export default Route;
